09b64dee6fd26c51484ab1c05c0dccadfcc17b7a
Soyoung Jo <67154580+jo-soyoung@users.noreply.github.com>
2021-02-07 00:16:08 +0900

    Add files
    html
    - index.html
    - about.html
    
    css
    - style.css
    
    js
    - app.js
    - skill-bar.js
    - slider.js
    
    assets
    - images

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Menu Control
+const burgerBtn = document.querySelector('.hamburger')
+const header = document.querySelector('header')
+
+burgerBtn.addEventListener('click', () => {
+    header.classList.toggle('open')
+})
+
+
+// Cursor Customizing Control
+if (window.innerWidth > 1440) {
+    const mouseCursor = document.querySelector('.cursor');
+    const workPics = document. querySelectorAll('.work-pic');
+
+    for(let workPic of workPics) {
+        workPic.addEventListener('mouseover', () => {
+            mouseCursor.classList.remove('no-display');
+        })
+        workPic.addEventListener('mouseleave', () => {
+            mouseCursor.classList.add('no-display');
+        })
+
+        workPic.addEventListener('mousemove', cursor);
+        function cursor(e) {
+            mouseCursor.style.top = `${e.pageY}px`;
+            mouseCursor.style.left = `${e.pageX}px`;
+        }
+    }
+}
---
+++ js/slider.js
@@ -0,0 +1,163 @@
+if (window.innerWidth > 1440) {
+  const worklist = document.querySelector('.work-list');
+  const works = document.querySelectorAll('.work');
+  const details = document.querySelectorAll('.work-detail');
+  const titles = document.querySelectorAll('.work-title-list .work-title');
+  const pagers = document.querySelectorAll('.pagers li');
+  // const btns = document.querySelectorAll("button");
+  
+  const wLength = works.length;
+  let count = 0;
+  let delay = 1000;
+  
+  // const [prev, next] = btns;
+  
+  
+  // Btn Control
+  // next.addEventListener("click", showNext);
+  // prev.addEventListener("click", showPrev);
+  
+  
+  // Detail Fade Control
+  function detailShow(i) {
+    details[i].style.opacity = 1;
+  }
+  
+  function detailFade(i) {
+    details[i].style.opacity = 0;
+  }
+  
+  
+  // OOP
+  class Animate {
+    constructor(current){
+      this.current = current;
+    }
+    upward() {
+      this.current.style.transform = `translate(-50%, -350%) rotate(-10deg)`;
+    }
+    downward() {
+      this.current.style.transform = `translate(-50%, -50%) rotate(10deg)`;
+    }
+    move(arg) {
+      arg.style.transform = `translate(-70%, -200%)`;
+    }
+  }
+  const slide = new Animate(works[count]);
+
+  class Animate2 {
+    constructor(current){
+      this.current = current;
+    }
+    upward() {
+      this.current.style.transform = `translateY(0%)`;
+    }
+    downward() {
+      this.current.style.transform = `translateY(1500%)`;
+    }
+    middle(arg) {
+      arg.style.transform = `translateY(900%)`;
+    }
+  }
+  const slideTitle = new Animate2(titles[count]);
+  
+  
+  // Init
+  (() => {
+      for (let i = 0; i < wLength; i++) {
+        works[i].style.transition = `${delay / 2}ms ease`;
+        titles[i].style.transition = `${delay / 2}ms ease`;
+      }
+  
+      if (count === 0) {
+        slideTitle.middle(titles[count]);
+        slide.move(works[count]);
+        setTimeout(() => {
+          detailShow(count);
+        }, delay / 2);
+      }
+
+      worklist.querySelectorAll('.work-title').forEach(function (title) {
+        title.classList.add('sr-only');
+      })
+  })();
+  
+  
+  // Show Next Slider
+  function showNext() {
+    if (count === wLength - 1) {
+      return
+    } else {
+      let currentSlide = works[count];
+      let currentTitle = titles[count];
+      const slide = new Animate(currentSlide);
+      const slideTitle = new Animate2(currentTitle);
+  
+      // current slider
+      slideTitle.downward();
+      slide.upward();
+      detailFade(count);
+      pagerColor(count + 1);
+      pagerDecolor(count);
+  
+      // next slider
+      slideTitle.middle(titles[count + 1]);
+      slide.move(works[count + 1]);
+      setTimeout(() => {
+        detailShow(count);
+      }, delay / 2);
+      
+      count++;
+    }
+  }
+  
+  // Show Prev Slider
+  function showPrev() {
+    if (count === 0) {
+      return
+    } else {
+      let currentSlide = works[count];
+      let currentTitle = titles[count];
+      const slide = new Animate(currentSlide);
+      const slideTitle = new Animate2(currentTitle);
+
+      // current slider
+      slideTitle.upward();
+      slide.downward();
+      detailFade(count);
+      pagerColor(count - 1);
+      pagerDecolor(count);
+  
+      // previous slider
+      slideTitle.middle(titles[count - 1]);
+      slide.move(works[count - 1]);
+      setTimeout(() => {
+        detailShow(count);
+      }, delay / 2);
+  
+      count--;
+    }
+  }
+  
+  
+  // Handle Slider Scroll
+  function handleScroll(e) {
+    if(e.deltaY < 0) {
+      showPrev();
+    } else {
+      showNext();
+    }
+  }
+
+  window.addEventListener('wheel', handleScroll)
+  
+  
+  // Handle Pagers Color
+  function pagerColor(i) {
+    pagers[i].classList.add('active');
+  }
+  
+  function pagerDecolor(i) {
+    pagers[i].classList.remove('active');
+  }  
+}
